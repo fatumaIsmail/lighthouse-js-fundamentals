08e195e6e430c2b378e7aebbd9753866c029f6eb
fatumaIsmail <fatumaismail38@gmail.com>
2021-09-11 14:06:34 -0400

    modified file by adding the question to the problem as a comment

--- whichSchool.js
+++ whichSchool.js
@@ -1,3 +1,11 @@
+// The logic should help learners decide which school they should attend. Replace the comment inside the function with your code.
+
+// The function returns one of the following strings, based on the value of age:
+
+// Elementary School if age is below 13
+// Secondary School if age is between 13 and 18 (both inclusive)
+// Lighthouse Labs in all other cases.
+
 const whichSchool = function (age) {
   if (age < 13) {
     return "Elementary School";
