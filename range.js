827778b1822b8ceb59255d4dab05d1a615a5f4e2
fatumaIsmail <fatumaismail38@gmail.com>
2021-09-11 19:19:30 -0400

    function that takes 3 integer parameters that returns an array of numbers from start to end counting by step

---
+++ range.js
@@ -0,0 +1,25 @@
+const range = function (start, end, step) {
+  let output = [];
+  if (
+    typeof start === "undefined" &&
+    typeof end === "undefined" &&
+    typeof step === "undefined"
+  ) {
+    return output;
+  } else if (start > end) {
+    return output;
+  } else if (step <= 0) {
+    return output;
+  } else {
+    for (let i = start; i <= end; i += step) {
+      output.push(i);
+    }
+  }
+
+  return output;
+};
+
+console.log(range(0, 10, 2));
+console.log(range(10, 30, 5));
+console.log(range(-5, 2, 3));
+console.log(range(20, 5, 0));
