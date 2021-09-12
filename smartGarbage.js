c7f6bef09bec10c6a64896eb8939ea4ec5f5a4b9
fatumaIsmail <fatumaismail38@gmail.com>
2021-09-12 18:37:51 -0400

    cleaned file

--- smartGarbage.js
+++ smartGarbage.js
@@ -9,12 +9,3 @@ const smartGarbage = function (trash, bins) {
 
 console.log(smartGarbage("recycling", { waste: 4, recycling: 2, compost: 5 }));
 console.log(smartGarbage("waste", { waste: 4, recycling: 2, compost: 5 }));
-
-// returns object
-// {
-//   waste: 4,
-//   recycling: 3,
-//   compost: 5
-// }
-
-// function working correctly but evaluator marked as wrong. What is the issue???
