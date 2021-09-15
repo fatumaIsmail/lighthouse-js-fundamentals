d8ea714232059b4e3d667e2aac2cd3c6bdd28b04
fatumaIsmail <fatumaismail38@gmail.com>
2021-09-15 19:16:06 -0400

    created function that takes an array of instructors and determines which instructor has the longest name

---
+++ longestName.js
@@ -0,0 +1,30 @@
+// In this exercise, we will be given a list of instructors and have to determine which instructor has the longest name.
+
+const instructorWithLongestName = function (instructors) {
+  let length = 0;
+  let longest;
+  for (let i = 0; i < instructors.length; i++) {
+    if (instructors[i].name.length > length) {
+      length = instructors[i].name.length;
+      longest = instructors[i];
+    }
+  }
+  return longest;
+};
+
+console.log(
+  instructorWithLongestName([
+    { name: "Samuel", course: "iOS" },
+    { name: "Jeremiah", course: "Web" },
+    { name: "Ophilia", course: "Web" },
+    { name: "Donald", course: "Web" },
+  ])
+);
+
+console.log(
+  instructorWithLongestName([
+    { name: "Matthew", course: "Web" },
+    { name: "David", course: "iOS" },
+    { name: "Domascus", course: "Web" },
+  ])
+);
